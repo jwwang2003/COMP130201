1b6ce1ac9259d6b4d760394af53ee497af986380
Jimmy Wang <jwwang.03@outlook.com>
2023-06-04 17:17:53 +0800

    updated lab4

--- lab4cachelab/cachelab-handout/csim.c
+++ lab4cachelab/cachelab-handout/csim.c
@@ -1,7 +1,248 @@
 #include "cachelab.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <getopt.h>
+#include <math.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
-    return 0;
+#define DEBUG 0
+
+typedef struct _line {
+	int valid;
+	unsigned long int tag;
+	int age;
+} line;
+
+FILE *tracefile; 
+
+extern char *optarg; 
+extern int optind;
+
+int LRU(line **cacheArray, int sets, int lines, int usedSet) {
+	int j, oldest_line, oldest_age = -1;
+
+	for (j = 0; j < lines; j++) {
+		if ((cacheArray[usedSet][j]).age > oldest_age) {
+			oldest_age = (cacheArray[usedSet][j]).age;
+			oldest_line = j;
+		}
+	}
+
+	return oldest_line;
+}
+
+void updateAge(line **cacheArray, int sets, int lines, int usedSet, int usedLine) {
+	int j;
+	for (j = 0; j < lines; j++) {
+		if (j == usedLine)
+			(cacheArray[usedSet][j]).age = 0;
+		else 
+			(cacheArray[usedSet][j]).age += 1;
+	}
+}
+
+void printAges(line **cacheArray, int sets, int lines) {
+	int i, j;
+	for (i = 0; i < sets; i ++) {
+		for (j = 0; j < lines; j++) {
+			printf("%d ", (cacheArray[i][j]).age );
+		}
+		printf("\n");
+	}
+}
+
+void clean(line ** cacheArray, int sets, int lines) {
+	int i;
+	for (i = 0; i < sets; i++) {
+		free(cacheArray[i]);
+	}
+	free (cacheArray);
+}
+
+int main(int argc, char** argv) {
+	int hit_count = 0, miss_count = 0, eviction_count = 0;
+	int i;
+	int opt;
+	int VERBOSE_MODE = 0; 
+	int s; 
+	int numSets;
+	int E;
+	int b; 
+	char * t = "default";
+
+	struct _line **cacheArray; 
+	unsigned long int addr, set, tag; 
+	int bool_hit, bool_evict; 
+	int first_invalid_line, lru_line; 
+
+	char line[32]; 
+	const char delimiters[] = ", ";
+	char *token_ins, *token_addr, *token_size;
+	int size;
+
+	while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+		switch (opt) {
+			case 'v': VERBOSE_MODE = 1; break;
+			case 's': s = atoi(optarg); break;
+			case 'E': E = atoi(optarg); break;
+			case 'b': b = atoi(optarg); break;
+			case 't': t = optarg; break;
+
+			case 'h':
+			default:
+					fprintf(stderr, "Usage: %s [-hvsEbt] [tracefile...]\n", argv[0]);
+					exit(EXIT_FAILURE);
+		}
+	}
+
+	if (DEBUG) {
+		printf("%s\n", "Result from command line arguments:");
+		printf("Verbose mode: %d \ns: %d \nE: %d \nb: %d \nt: %s\n",VERBOSE_MODE, s, E, b, t);
+	}
+
+	tracefile = fopen(t , "r");
+	if(tracefile == NULL) {
+		printf("Error opening file");
+		return(-1);
+	}
+
+	numSets = pow(2, s);
+
+	cacheArray = malloc(numSets*sizeof(struct _line*));
+
+	if (cacheArray == NULL) {
+		printf("%s\n", "Malloc error");
+		return 1;
+	}
+
+	for (i = 0; i < numSets; i++) {
+		cacheArray[i] = malloc( E*sizeof(struct _line) );
+		if (cacheArray[i] == NULL) {
+			printf("%s\n", "Malloc error");
+			return 1;
+		}
+	}
+
+	while(fgets(line, 32, tracefile) != NULL) {
+
+		bool_hit = 0; 
+		bool_evict = 0;
+		first_invalid_line = 0;
+
+		if (line[0] == ' ') {
+
+			token_ins = strtok (line, delimiters); 
+			token_addr = strtok (NULL, delimiters); 
+			token_size = strtok (NULL, delimiters);
+			size = atoi(token_size);
+			if (VERBOSE_MODE) printf("%s %s,%d ", token_ins, token_addr, size); 
+
+			addr = strtol(token_addr, NULL, 16); 
+			tag = (addr >> s) >> b; 
+			set = ((addr << (64 - s - b)) >> (64 - s)); 
+
+			if (DEBUG) {
+				printf("Address in hex: %lx\n", addr);
+				printf("Tag in hex: %lx\n", tag);
+				printf("Set in hex: %lx\n", set);
+				printf("Set index: %ld\n", set);
+			}
+
+			for (i = 0; i < E; i++) {
+				if ( (cacheArray[set][i]).valid == 1 && (cacheArray[set][i]).tag == tag ) {
+					bool_hit = 1; 
+					updateAge(cacheArray, numSets, E, set, i); 
+					break;
+				}
+				else if (!((cacheArray[set][i]).valid) && !first_invalid_line) { 
+					first_invalid_line = i+1; 
+				}
+			}
+
+			if (!bool_hit) {
+
+				if (first_invalid_line) {
+					bool_evict = 0;
+					(cacheArray[set][first_invalid_line-1]).valid = 1;
+					(cacheArray[set][first_invalid_line-1]).tag = tag;
+					updateAge(cacheArray, numSets, E, set, first_invalid_line-1); 
+				}
+
+				else {
+					bool_evict = 1;
+					lru_line = LRU(cacheArray, numSets, E, set);
+					(cacheArray[set][lru_line]).valid = 1;
+					(cacheArray[set][lru_line]).tag = tag;
+					updateAge(cacheArray, numSets, E, set, lru_line); 
+				}
+			}
+
+
+			switch (*token_ins) {
+
+				case 'M': 
+					if (bool_hit) {
+						hit_count +=2;
+						if (VERBOSE_MODE) printf("hit hit\n");
+					}
+					else if (bool_evict) {
+						miss_count +=1;
+						eviction_count += 1;
+						hit_count +=1;
+						if (VERBOSE_MODE) printf("miss eviction hit\n");
+					}
+					else {
+						miss_count +=1;
+						hit_count +=1;
+						if (VERBOSE_MODE) printf("miss hit\n");
+					}
+					break;
+
+				case 'L':
+					if (bool_hit) {
+						hit_count +=1;
+						if (VERBOSE_MODE) printf("hit\n");
+					}
+					else if (bool_evict) {
+						miss_count +=1;
+						eviction_count += 1;
+						if (VERBOSE_MODE) printf("miss eviction\n");
+					}
+					else {
+						miss_count +=1;
+						if (VERBOSE_MODE) printf("miss\n");
+					}
+					break;
+
+				case 'S': 
+					if (bool_hit) {
+						hit_count +=1;
+						if (VERBOSE_MODE) printf("hit\n");
+					}
+					else if (bool_evict) {
+						miss_count +=1;
+						eviction_count += 1;
+						if (VERBOSE_MODE) printf("miss eviction\n");
+					}
+					else {
+						miss_count +=1;
+						if (VERBOSE_MODE) printf("miss\n");
+					}
+					break;
+			}
+
+			if (DEBUG) printAges(cacheArray, numSets, E);
+
+		}
+	}
+
+	printSummary(hit_count, miss_count, eviction_count);
+	fclose(tracefile);
+	clean(cacheArray, numSets, E);
+
+	return 0;
 }
--- lab4cachelab/cachelab-handout/trans.c
+++ lab4cachelab/cachelab-handout/trans.c
@@ -22,29 +22,145 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+  /*
+   we will navigate three cases in this function: 
+   1) square matrix of size 32 
+   2) square matrix of size 64
+   3) when matrix is anything else (for example: 61*67)
+   */
+
+int blockSize; //variable for size of block, used in each of the iterations, N ==32, N ==63 and the else 
+int blockForRow, blockForCol; //to iterate over blocks, user in outer loops
+int r, c; //to iterate through each block, used in inner loops
+int temp = 0, d = 0; //d stands for diagonal, temp is just a temporary variable
+int v0,v1,v2,v3,v4; //Variables to be used in the N==64 case for various assignments within it
+	/*
+	Using blockSize = 8 in this case. Only N == 32 is used in the condition since matrix transpose can
+	occur for any a*b and c*a where only a needs to be same and b and c can vary.
+	Blocking is used here. 
+	4 levels of loop sare used here. 2 outer loops iterate accross blocks (in column major iteration) while the 2 inner loops iterate through each block. 
+	*/
+	if (N == 32)
+	{
+		blockSize = 8;
+		for(blockForCol = 0; blockForCol < N; blockForCol += 8)
+		{
+			for(blockForRow = 0; blockForRow < N; blockForRow += 8)
+			{
+				for(r = blockForRow; r < blockForRow + 8; r++)
+				{
+					for(c = blockForCol; c < blockForCol + 8; c++)
+					{
+						//Row and column are not equal
+						if(r != c)
+						{
+							B[c][r] = A[r][c];
+						}
+						
+						else 
+						{
+						//Store in temp instead of missing in B[j][i] to decrease misses
+						temp = A[r][c];
+						d = r;
+						}
+					}
+					//We don't move elements on diagonals since we are transposing a square matrix
+					if (blockForRow == blockForCol)	
+					{
+						B[d][d] = temp;
+					}
+				}
+			}
+		}
+	}
+
+	/* Using blockSize = 4 here. 
+	2 levels of loops are used 
+	We assign elements in each row individually. Causes reduced missess. */
+	else if (N == 64)
+	{	
+		transpose_opt(M, N, A, B);
+ 		// blockSize = 4;
+		// for(r = 0; r < N; r += blockSize)
+		// {
+		// 	for(c = 0; c < M; c += blockSize)
+		// 	{
+		// 		/*Elements in A[r][], A[r+1][], A[r+2][] are assigned to the variables for use throughout this loop
+		// 		This is becuase we are only allowed to modify the second matrix B but not the matrix A */
+		// 		v0 = A[r][c];
+		// 		v1 = A[r+1][c];
+		// 		v2 = A[r+2][c];
+		// 		v3 = A[r+2][c+1];
+		// 		v4 = A[r+2][c+2];
+		// 		//Elements in B[c+3][] are assigned
+		// 		B[c+3][r] = A[r][c+3];
+		// 		B[c+3][r+1] = A[r+1][c+3];
+		// 		B[c+3][r+2] = A[r+2][c+3];
+		// 		//Elements in B[c+2][] are assigned 
+		// 		B[c+2][r] = A[r][c+2];
+		// 		B[c+2][r+1] = A[r+1][c+2];
+		// 		B[c+2][r+2] = v4;
+		// 		v4 = A[r+1][c+1];
+		// 		//Elements in B[c+1][] are assigned
+		// 		B[c+1][r] = A[r][c+1];
+		// 		B[c+1][r+1] = v4;
+		// 		B[c+1][r+2] = v3;
+		// 		//Elements in B[c][] are assigned
+		// 		B[c][r] = v0;
+		// 		B[c][r+1] = v1;
+		// 		B[c][r+2] = v2;
+		// 		//Elements in row A[r+3][] are assigned to the left out elements in B (where B has r+3)
+		// 		B[c][r+3] = A[r+3][c];
+		// 		B[c+1][r+3] = A[r+3][c+1];
+		// 		B[c+2][r+3] = A[r+3][c+2];
+		// 		v0 = A[r+3][c+3];
+		// 		//Finally, elements in row B[c+3][] are assigned
+		// 		B[c+3][r+3] = v0;
+		// 	}
+		// }
+	}
+
+	/* This is the case for a random matrix size. We use blockSize = 16 
+	2 levels of loops are used to iterate over blocks in column major iteration and 2 levels are used to go through the blocks	*/
+	else 
+	{
+		blockSize = 16;
+		
+		for (blockForCol = 0; blockForCol < M; blockForCol += blockSize)
+		{
+			for (blockForRow = 0; blockForRow < N; blockForRow += blockSize)
+			{	
+				/*Since our sizes can be odd, not all blocks will be square. Special case: if (blockForRow + 16 > N), we get an invalid access. 
+				We also do regular check for i<N and j<M */
+				for(r = blockForRow; (r < N) && (r < blockForRow + blockSize); r++)
+				{
+					for(c = blockForCol; (c < M) && (c < blockForCol + blockSize); c++)
+					{
+						//row and column are not same
+						if (r != c)
+						{
+							B[c][r] = A[r][c];
+						}
+						
+						//row and column same 
+						else
+						{
+							temp = A[r][c];
+							d = r;
+						}
+					}
+					
+					//Row and column number are same in the blocks, diagonal element assigned
+					if(blockForRow == blockForCol) 
+					{
+						B[d][d] = temp;
+					}
+				}
+			}
+		}
+	}
 }
 
-/* 
- * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
-
-/* 
- * trans - A simple baseline transpose function, not optimized for the cache.
- */
-char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
-}
 
 /*
  * registerFunctions - This function registers your transpose
@@ -57,9 +173,10 @@ void registerFunctions()
 {
     /* Register your solution function */
     registerTransFunction(transpose_submit, transpose_submit_desc); 
+	//Used only 1 function for all cases
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    // registerTransFunction(trans, trans_desc); 
 
 }
 
@@ -82,3 +199,67 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
     return 1;
 }
 
+void transpose_opt(int M, int N, int A[N][M], int B[M][N])
+{
+	int interval = 8;
+	int gap = 4;
+	int startX,startY;	
+	for (startY = 0; startY < M; startY += interval)
+		for (startX = 0; startX < N; startX += interval){
+			int nowX = startY;
+			int nowY = startX;
+			
+			int nextX = nowX;
+			int nextY = nowY;
+			
+			do{ 
+				nextY += interval;
+				if (nextY >= N){
+					nextX += interval;
+					nextY -= N;
+				}
+			} while (startY == nextY);
+
+			int nextX_2 = nextX;
+			int nextY_2 = nextY;
+
+			do{ 
+				nextY_2 += interval;
+				if (nextY_2 >= N){
+					nextX_2 += interval;
+					nextY_2 -= N;				
+				}
+			} while (startY == nextY_2);
+
+			if (nextX >= M){
+				for (int i = 0; i < interval; i++)
+					for (int j = 0; j < interval; j++)
+						B[nowX + j][nowY + i] = A[startX + i][startY + j];			
+			} else {
+				for (int i = 0; i < gap; i++)
+					for (int j = 0; j < interval; j++)
+						B[nextX + i][nextY + j] = A[startX + i][startY + j];			
+				
+				for (int i = 0; i < gap; i++)
+					for (int j = 0; j < interval; j++)
+						B[nextX_2 + i][nextY_2 + j] = A[startX + gap + i][startY + j];		
+
+				for (int i = 0; i < gap; i++)
+					for (int j = 0; j < gap; j++){
+						B[nowX + j][nowY + i] = B[nextX + i][nextY + j];
+						B[nowX + j][nowY + gap + i] = B[nextX_2 + i][nextY_2 + j];	
+					}
+				
+				for (int i = 0; i < gap; i++)
+					for (int j = 0; j < gap; j++){
+						B[nowX + gap + j][nowY + i] = B[nextX + i][nextY + gap + j];
+						B[nowX + gap + j][nowY + gap + i] = B[nextX_2 + i][nextY_2 + gap + j];	
+					}			
+			}
+			
+		}		
+			
+			
+		
+
+}
